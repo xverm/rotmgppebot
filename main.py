22cedde9550eaaaa9de58b2c9fa47fc37df03f8e
xverm <vintrx829@gmail.com>
2026-06-10 10:54:02 -0400

    Update main.py

--- main.py
+++ main.py
@@ -73,11 +73,9 @@ from utils.memory_hygiene import run_memory_hygiene
 
 from utils.autocomplete import class_autocomplete, item_name_autocomplete, bonus_autocomplete, user_bonus_autocomplete, target_user_bonus_autocomplete, team_name_autocomplete, rarity_autocomplete
 
-SERVER1_ID = 879497062117412924 # Last Oasis
-SERVER2_ID = 1435436110829326459 # Test Server
-SERVER3_ID = 1485395885666992248 # My Testing Server
+SERVER1_ID = 1514246572127551488 # Last Oasis
 
-guilds = [discord.Object(id=SERVER1_ID), discord.Object(id=SERVER2_ID), discord.Object(id=SERVER3_ID)]
+guilds = [discord.Object(id=SERVER1_ID)]
 
 load_dotenv()
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
